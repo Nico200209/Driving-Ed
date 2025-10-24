a7d45e87f8e5c2b914cc300a2c671fae04e16678
Nicolás García <espallemann@gmail.com>
2025-10-24 16:12:35 +0200

    Improves car controller with CarStats and brake
    Introduces a CarStats scriptable object to manage car properties such as power.
    Updates the car controller to use the CarStats object.
    Implements braking by using spacebar.
    Implements wheel turning lerp speed
    Introduces Handbreak

--- Assets/Scripts/Car/CarController.cs
+++ Assets/Scripts/Car/CarController.cs
@@ -16,40 +16,83 @@ public enum WheelType
 }
 public class CarController : MonoBehaviour
 {
-    public Wheel[] wheels;
-    public Vector2 moveInput;
-    public float powerMultiplier = 1f;
+    public CarStats carStats;
+    public Wheel[] wheels; //ToDo: Know how many wheels are for steering, and how many for hand break
+
 
     public float maxSteer = 30f;
     public float wheelbase = 2.5f; // Distance between front and rear axles
     public float trackwidth = 1.5f; // Distance between left and right
+    public bool SpacebarPressed;
 
-    public void OnMove(InputValue value) => moveInput = value.Get<Vector2>();
+
+    #region common 
+    private Vector2 moveInput;
+    public float wheelTurnLerpSpeed = 1f;
+
+    #endregion
+
+    void Start()
+    {
+        carStats = GetComponent<CarStats>();
+    }
+
+    public void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    public void OnJump(InputValue input)
+    {
+        SpacebarPressed = input.Get<float>() == 1; //Set to true if spacebar is pressed
+    }
 
     void FixedUpdate()
     {
+
+        SpacebarPressed = Input.GetKey(KeyCode.Space); //Set to true if spacebar is pressed
+
+
+        if (!carStats) return;
+
         //ToDo: Check if wheels are present
 
-        foreach (var wheel in wheels)
+        for (int i = 0; i < wheels.Length; i++)
         {
-            wheel.collider.motorTorque = moveInput.y * powerMultiplier;
+            if (i > 1)
+            {
+                //Rear Wheels
+                wheels[i].collider.motorTorque = SpacebarPressed ? 0 : moveInput.y * carStats.MaxPowerNM;
+                wheels[i].collider.brakeTorque = !SpacebarPressed ? 0 : 1000;
+
+            }
+            else
+            {
+                //Front Wheels
+                wheels[i].collider.motorTorque = moveInput.y * carStats.MaxPowerNM;
+
+            }
+
         }
+
+
+
         float steer = moveInput.x * maxSteer;
 
         //ToDo: Implement Math Lerp
         if (moveInput.x > 0) //If turning right
         {
-            wheels[0].collider.steerAngle = Mathf.Rad2Deg * Mathf.Atan(wheelbase / (trackwidth / 2 + Mathf.Tan(Mathf.Deg2Rad * steer) * wheelbase));
-            wheels[1].collider.steerAngle = steer;
+            wheels[0].collider.steerAngle = Mathf.Lerp(wheels[0].collider.steerAngle, Mathf.Rad2Deg * Mathf.Atan(wheelbase / (trackwidth / 2 + Mathf.Tan(Mathf.Deg2Rad * steer) * wheelbase)), Time.deltaTime * wheelTurnLerpSpeed);
+            wheels[1].collider.steerAngle = Mathf.Lerp(wheels[1].collider.steerAngle, steer, Time.deltaTime * wheelTurnLerpSpeed);
         }
         else if (moveInput.x < 0) //If turning left
         {
-            wheels[0].collider.steerAngle = steer;
-            wheels[1].collider.steerAngle = Mathf.Rad2Deg * Mathf.Atan(wheelbase / (-trackwidth / 2 + Mathf.Tan(Mathf.Deg2Rad * steer) * wheelbase));
+            wheels[0].collider.steerAngle = Mathf.Lerp(wheels[0].collider.steerAngle, steer, Time.deltaTime * wheelTurnLerpSpeed);
+            wheels[1].collider.steerAngle = Mathf.Lerp(wheels[1].collider.steerAngle, Mathf.Rad2Deg * Mathf.Atan(wheelbase / (-trackwidth / 2 + Mathf.Tan(Mathf.Deg2Rad * steer) * wheelbase)), Time.deltaTime * wheelTurnLerpSpeed);
         }
         else //If not turning
         {
-            wheels[0].collider.steerAngle = wheels[1].collider.steerAngle = 0;
+            wheels[0].collider.steerAngle = wheels[1].collider.steerAngle = Mathf.Lerp(wheels[0].collider.steerAngle = wheels[1].collider.steerAngle, 0, Time.deltaTime * wheelTurnLerpSpeed);
         }
         for (int i = 0; i < wheels.Length; i++)
         {
@@ -60,7 +103,7 @@ public class CarController : MonoBehaviour
 
             Transform[] ChildTransforms = new Transform[wheels[i].collider.transform.childCount];
             int index = 0;
-            foreach(var item in ChildTransforms)
+            foreach (var item in ChildTransforms)
             {
                 wheels[i].collider.transform.GetChild(index).rotation = Rot;
                 wheels[i].collider.transform.GetChild(index).position = Pos;
